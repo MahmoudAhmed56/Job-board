47da41ad023b88df663adac169f34f850c929ffa
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-20 00:51:43 +0200

    add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import H1 from "@/components/ui/h1";
+
+const Error = () => {
+  return (
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3 space-y-5">
+      <H1>Error</H1>
+      <p>Unexpected error occurred</p>
+    </main>
+  )
+}
+
+export default Error
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import JobFilterSidebar from "@/components/JobFilterSidebar";
 import JobListItem from "@/components/JobListItem";
+import H1 from "@/components/ui/h1";
 import prisma from "@/lib/prisma";
 
 
@@ -15,7 +16,7 @@ export default async function Home() {
   return (
    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
     <div className="space-y-5 text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">Developer jobs</h1>
+        <H1>Developer jobs</H1>
         <p className="text-muted-foreground">Find your dream job.</p>
       </div>
   <section className="flex flex-col md:flex-row gap-4">
