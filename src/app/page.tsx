0cf65e1127422ce792a640e101af3dbcd018c7e0
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-20 01:43:14 +0200

    add search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
 import JobFilterSidebar from "@/components/JobFilterSidebar";
 import JobResults from "@/components/JobResults";
 import H1 from "@/components/ui/h1";
+import { jobFilterValues } from "@/lib/validation";
 
-export default async function Home() {
+interface pageProps {
+  searchParams: {
+    q?: string;
+    type?: string;
+    location?: string;
+    remote?: string;
+  };
+}
+
+export default async function Home({
+  searchParams: { location, q, remote, type },
+}: pageProps) {
+  const filterValues: jobFilterValues = {
+    q,
+    location,
+    remote:remote === "true",
+    type,
+  }
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
@@ -11,7 +29,7 @@ export default async function Home() {
       </div>
       <section className="flex flex-col md:flex-row gap-4">
         <JobFilterSidebar />
-        <JobResults />
+        <JobResults filterValues={filterValues} />
       </section>
     </main>
   );
--- src/components/JobResults.tsx
+++ src/components/JobResults.tsx
@@ -1,22 +1,52 @@
-import prisma from "@/lib/prisma"
-import JobListItem from "./JobListItem"
+import prisma from "@/lib/prisma";
+import JobListItem from "./JobListItem";
+import { jobFilterValues } from "@/lib/validation";
+import { Prisma } from "@prisma/client";
 
-const JobResults = async () => {
-  const jobs = await prisma.job.findMany({
-    where:{
-      approved:true
-    },
-    orderBy:{
-      createdAt: "desc"
-    }
-  })
-  return (
-    <div className="grow space-y-4">
-    {jobs.map((job)=>(
-      <JobListItem job={job} key={job.id} />
-      ))}
-  </div>
-  )
+interface JobResultsProps {
+  filterValues: jobFilterValues;
 }
 
-export default JobResults
+const JobResults = async ({
+  filterValues: { location, q, remote, type },
+}: JobResultsProps) => {
+  const searchString = q
+    ?.split(" ")
+    .filter((word) => word.length > 0)
+    .join(" & ");
+  const searchFilter: Prisma.JobWhereInput = searchString
+    ? {
+        OR: [
+          { title: { search: searchString } },
+          { companyName: { search: searchString } },
+          { type: { search: searchString } },
+          { locationType: { search: searchString } },
+          { location: { search: searchString } },
+        ],
+      }
+    : {};
+  const where: Prisma.JobWhereInput = {
+    AND: [
+      searchFilter,
+      type ? { type } : {},
+      location ? { location } : {},
+      remote ? { locationType: "remote" } : {},
+      { approved: true },
+    ],
+  };
+  const jobs = await prisma.job.findMany({
+    where,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return (
+    <div className="grow space-y-4">
+      {jobs.map((job) => (
+        <JobListItem job={job} key={job.id} />
+      ))}
+    </div>
+  );
+};
+
+export default JobResults;
