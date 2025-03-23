20865d52e24329ce16f82316d68eb4d977cb9c61
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-23 23:32:27 +0200

    add layout to admin page to provide clerk auth

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin",
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>){
+  return (
+    <ClerkProvider>
+    {children}
+  </ClerkProvider>
+  );
+}
