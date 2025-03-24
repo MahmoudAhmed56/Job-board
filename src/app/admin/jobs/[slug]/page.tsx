4eaaa9fa7ba9b7d8364a231073eeb648fa106717
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-25 01:51:13 +0200

    fix some ts errors9

--- src/app/admin/jobs/[slug]/page.tsx
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -4,17 +4,17 @@ import { notFound } from "next/navigation";
 import AdminSidebar from "./AdminSidebar";
 
 interface PageProps {
-  params: Promise<{ slug: string }>;
+  params: { slug: string };
 }
 
 const Page = async ({ params }: PageProps) => {
-  // Await the params before destructuring
-  const { slug } = await params;
-  
+  const { slug } = params; // No need to await
+
   const job = await prisma.job.findUnique({
     where: { slug },
   });
   if (!job) notFound();
+
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
       <JobPage job={job} />
