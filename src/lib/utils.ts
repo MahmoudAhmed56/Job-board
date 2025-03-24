e709b29bf78a3ce982577aa9cd1fb86e3272876a
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-24 16:16:57 +0200

    add approveSubmission action

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { formatDistanceToNowStrict } from "date-fns";
+import { UserResource } from "@clerk/types"
+import { User } from "@clerk/nextjs/server"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -20,4 +22,8 @@ export function relativeDate(from: Date) {
 
 export function toSlug(str:string) {
   return str.toLowerCase().replace(/ /g,"-").replace(/[^\w-]+/g,"")
+}
+
+export function isAdmin(user:UserResource | User) {
+  return user.publicMetadata?.role === "admin"
 }
