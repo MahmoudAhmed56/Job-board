25d8c296690d2a0ff97faddd101671711db8f919
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-24 16:36:02 +0200

    add deleteJob action

--- src/app/admin/actions.ts
+++ src/app/admin/actions.ts
@@ -1,7 +1,10 @@
+"use server"
 import prisma from "@/lib/prisma";
 import { isAdmin } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
+import { del } from "@vercel/blob";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 type FormState = { error?: string } | undefined;
 
@@ -30,3 +33,32 @@ export async function approveSubmission(formData: FormData):Promise<FormState> {
   }
 }
 
+
+export async function deleteJob(formData:FormData):Promise<FormState>{
+  try {
+    const jobId = parseInt(formData.get("jobId")as string)
+    const user = await currentUser()
+    if(!user || !isAdmin(user)){
+      throw new Error("Not authorized");
+    }
+    const job = await prisma.job.findUnique({
+      where:{id:jobId}
+    })
+    if(job?.companyLogoUrl){
+      await del(job.companyLogoUrl)
+    }
+    await prisma.job.delete({
+      where:{
+        id:jobId
+      }
+    })
+    revalidatePath("/")
+  } catch (error) {
+    let message = "unexpected error";
+    if(error instanceof Error){
+      message = error.message
+    }
+    return{error: message}
+  }
+  redirect("/admin")
+}
