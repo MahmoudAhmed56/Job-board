b7aaaae27e07266e7ded6d83b6e7f4518a0f3b72
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-24 17:35:29 +0200

    add adminSidebar components

--- src/app/admin/jobs/[slug]/page.tsx
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import JobPage from "@/components/JobPage";
 import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
+import AdminSidebar from "./AdminSidebar";
 
 interface PageProps {
   params: { slug: string };
@@ -14,6 +15,7 @@ const Page = async ({ params: { slug } }: PageProps) => {
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
       <JobPage job={job} />
+      <AdminSidebar job={job} />
     </main>
   );
 };
