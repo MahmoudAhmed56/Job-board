ee2ab34c41cd1e50fa701256e63e244c89d32fd4
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-25 03:40:26 +0200

    fix some ts errors29

--- src/app/admin/jobs/[slug]/page.tsx
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -1,69 +1,9 @@
-import JobPage from "@/components/JobPage";
-import { Button } from "@/components/ui/button";
-import prisma from "@/lib/prisma";
-import { Metadata } from "next";
-import { notFound } from "next/navigation";
-import { cache } from "react";
+import React from 'react'
 
-// Update the interface to reflect that params is a promise.
-interface PageProps {
-  params: Promise<{ slug: string }>;
-}
-
-const getJob = cache(async (slug: string) => {
-  const job = await prisma.job.findUnique({
-    where: { slug },
-  });
-
-  if (!job) notFound();
-
-  return job;
-});
-
-// export async function generateStaticParams() {
-//   const jobs = await prisma.job.findMany({
-//     where: { approved: true },
-//     select: { slug: true },
-//   });
-
-//   return jobs.map(({ slug }) => ( slug ));
-// }
-
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
-  // Await the params promise to get the slug.
-  const { slug } = await params;
-  const job = await getJob(slug);
-
-  return {
-    title: job.title,
-  };
-}
-
-export default async function Page({ params }: PageProps) {
-  const { slug } = await params;
-  const job = await getJob(slug);
-
-  const { applicationEmail, applicationUrl } = job;
-
-  const applicationLink = applicationEmail
-    ? `mailto:${applicationEmail}`
-    : applicationUrl;
-
-  if (!applicationLink) {
-    console.error("Job has no application link or email");
-    notFound();
-  }
-
+const Page = () => {
   return (
-    <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
-      <JobPage job={job} />
-      <aside>
-        <Button asChild>
-          <a href={applicationLink} className="w-40 md:w-fit">
-            Apply now
-          </a>
-        </Button>
-      </aside>
-    </main>
-  );
+    <div>Page</div>
+  )
 }
+
+export default Page
